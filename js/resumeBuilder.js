33adbaa349faa620a3ac7aee3bf4f7c12651a095
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-08-22 12:49:20 -0400

    change education selector

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -96,10 +96,10 @@ var education = {
         if(education.onlineCourses.length > 0) {
             $("#education").append(HTMLonlineClasses);
             for (i = 0; i < education.onlineCourses.length; i++) {
-                $(".education-entry").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title));
-                $(".education-entry").append(HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school));
-                $(".education-entry").append(HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates));
-                $(".education-entry").append(HTMLonlineURL.replace("%data%", education.onlineCourses[i].url));
+                $("#education").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title));
+                $("#education").append(HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school));
+                $("#education").append(HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates));
+                $("#education").append(HTMLonlineURL.replace("%data%", education.onlineCourses[i].url));
             }
         }
     }
